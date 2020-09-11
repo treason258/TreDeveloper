2fe5abe0d1bfcb358f299fd5fa1f076476fe2e98
matengfei <matengfei@xin.com>
2020-09-11 16:13:21 +0800

    调整可以正常使用ButterKnife

--- app/src/main/java/com/mjiayou/developer/MainActivity.java
+++ app/src/main/java/com/mjiayou/developer/MainActivity.java
@@ -6,15 +6,28 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.mjiayou.trecorelib.util.HelloUtils;
+import com.mjiayou.trecorelib.util.ToastUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_hello)
+    TextView tv_hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         // tv_hello
-        ((TextView) findViewById(R.id.tv_hello)).setText(HelloUtils.getHI());
+        if (tv_hello != null) {
+            tv_hello.setText(HelloUtils.getHI());
+        } else {
+            ToastUtils.show("tv_hello == null");
+        }
     }
 }
